b66befe10a7fb9f225efd47493a5955e6d662a60
unknown <arafkarim01@gmail.com>
2017-11-07 15:05:53 +0600

    add waves effect

--- dist/js/bootstrapPlusPlus.js
+++ dist/js/bootstrapPlusPlus.js
@@ -1,3 +1,9 @@
+/*!
+ * BootstrapPlusPlus v1.0.0-beta (https://bootstrapplusplus.com)
+ * Copyright 2017 The BootstrapPlusPlus
+ * Licensed under MIT (https://github.com/BootstrapPlusPlus/BootstrapPlusPlus/blob/master/LICENSE.md)
+ */
+
 $(document).ready(function(){
 
   /* script for dropdown on hover */
@@ -13,6 +19,32 @@ $(document).ready(function(){
   });
   $('input').blur(function(){
     $('.form-group').removeClass('label-focus');
+  });
+
+  // Ripple (waves) effect
+  $(".waves").click(function(event) {
+  	var surface = $(this);
+
+  	if (surface.find(".waves-ink").length == 0) {
+      surface.prepend("<div class='waves-ink'></div>");
+    }
+
+  	var ink = surface.find(".waves-ink");
+
+  	ink.removeClass("animate");
+
+  	if (!ink.height() && !ink.width()) {
+  		var d = Math.max(surface.outerWidth(), surface.outerHeight());
+  		ink.css({height: d, width: d});
+  	}
+
+  	var x = event.pageX - surface.offset().left - (ink.width() / 2);
+  	var y = event.pageY - surface.offset().top - (ink.height() / 2);
+
+  	ink.css({
+  		top: y + 'px',
+  		left: x + 'px'
+  	}).addClass("animate");
   });
 
 });
